92fb1fdc75301114ba2b1f84b356150945a1659f
abdul-vista <abdul@v-learning.in>
2023-10-14 18:02:01 +0530

    product page

--- src/customer/components/ProductDetails/ProductDetails.jsx
+++ src/customer/components/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,9 @@
 
 import { useState } from "react";
-import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
+import Rating from '@mui/material/Rating';
+import { color } from './../Product/FilterData';
+import { Button } from "@mui/material";
 
 const product = {
   name: "Basic Tee 6-Pack",
@@ -35,14 +37,11 @@ const product = {
     { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
   ],
   sizes: [
-    { name: "XXS", inStock: false },
-    { name: "XS", inStock: true },
+
     { name: "S", inStock: true },
     { name: "M", inStock: true },
     { name: "L", inStock: true },
     { name: "XL", inStock: true },
-    { name: "2XL", inStock: true },
-    { name: "3XL", inStock: true },
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -130,51 +129,42 @@ export default function ProductDetails() {
               </div>
           </div>
           {/* Product info */}
-          <div className="lg: col-span-1 mx-auto max-w-2xl px-4 pb-16 sm:px-6 ">
-            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-                {product.name}
-              </h1>
+          <div className="lg: col-span-1 mx-auto max-w-2xl px-4 pb-16 sm:px-6 lg:max-w-7xl lg:px-8
+          lg:pb-24">
+            <div className="lg:col-span-2 ">
+              <h1 className="text-lg font-semibold lg:text-xl text-gray-900">
+              Hand cut and sewn locally              </h1>
+             <h1 className="text-lg  lg:text-xl text-gray-900 opacity-60 pt-1">The Basic Tee 6-Pack allows you to fully 
+              express your vibrant personality with three grayscale options </h1>
             </div>
 
             {/* Options */}
             <div className="mt-4 lg:row-span-3 lg:mt-0">
               <h2 className="sr-only">Product information</h2>
-              <p className="text-3xl tracking-tight text-gray-900">
-                {product.price}
-              </p>
+              <div className="flex space-x-5 items-center text-lg lg:text-xl mt-6 text-gray-900">
+                <p className=" font-semibold">
+                  ₹199
+                </p>
+                <p className=" opacity-50 line-through">
+                  ₹211 
+                </p>
+                <p className=" text-green-600">
+                  5% Off
+                </p>
+                
+              </div>
 
               {/* Reviews */}
-              <div className="mt-6">
-                <h3 className="sr-only">Reviews</h3>
-                <div className="flex items-center">
-                  <div className="flex items-center">
-                    {[0, 1, 2, 3, 4].map((rating) => (
-                      <StarIcon
-                        key={rating}
-                        className={classNames(
-                          reviews.average > rating
-                            ? "text-gray-900"
-                            : "text-gray-200",
-                          "h-5 w-5 flex-shrink-0"
-                        )}
-                        aria-hidden="true"
-                      />
-                    ))}
-                  </div>
-                  <p className="sr-only">{reviews.average} out of 5 stars</p>
-                  <a
-                    href={reviews.href}
-                    className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    {reviews.totalCount} reviews
-                  </a>
-                </div>
+              <div className="mt-6"><div className=" flex items-center space-x-3">
+              <Rating name="read-only" value={3.5} readOnly />
+              <p className=" opacity-50 text-sm">45453 Ratings</p>
+              <p className=" ml-3 opacity-50 text-sm font-medium
+               text-indigo-600 hover:text-indigo-500">54154 reviews</p></div>
               </div>
 
               <form className="mt-10">
                 {/* Colors */}
-                <div>
+                {/* <div>
                   <h3 className="text-sm font-medium text-gray-900">Color</h3>
 
                   <RadioGroup
@@ -213,7 +203,7 @@ export default function ProductDetails() {
                       ))}
                     </div>
                   </RadioGroup>
-                </div>
+                </div> */}
 
                 {/* Sizes */}
                 <div className="mt-10">
@@ -296,12 +286,11 @@ export default function ProductDetails() {
                   </RadioGroup>
                 </div>
 
-                <button
-                  type="submit"
-                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                <Button color="secondary" variant="contained"
+                sx={{px:"2rem",py:"1rem",bgcolor:"#9155fd"}}
                 >
-                  Add to bag
-                </button>
+                  Add to Cart
+                </Button>
               </form>
             </div>
 
